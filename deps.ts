4be72df58f92ebc67d61c4e6078fd26a4e959e69
Yuki Tanaka <uki00a@gmail.com>
2021-04-18 15:54:12 +0900

    bump: Upgrade deno to v1.9.0 (#30)

--- deps.ts
+++ deps.ts
@@ -1,7 +1,7 @@
-export { ensureDir } from "https://deno.land/std@0.84.0/fs/ensure_dir.ts";
-export * as path from "https://deno.land/std@0.84.0/path/mod.ts";
+export { ensureDir } from "https://deno.land/std@0.93.0/fs/ensure_dir.ts";
+export * as path from "https://deno.land/std@0.93.0/path/mod.ts";
 export { default as frontMatter } from "https://cdn.skypack.dev/front-matter@4.0.2?dts";
 export { default as marked } from "https://cdn.skypack.dev/marked@1.2.7?dts";
 export * as dejs from "https://deno.land/x/dejs@0.9.3/mod.ts";
 export { Feed } from "https://esm.sh/feed@4.2.1?no-check";
-export { default as htmlParser } from "https://jspm.dev/node-html-parser@3.1.2";
+export * as htmlParser from "https://esm.sh/node-html-parser@3.1.2?no-check";
