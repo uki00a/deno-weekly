e8e7bcd10b721c86f8665e081ac1e92743efeb49
uki00a <uki00a@gmail.com>
2021-08-21 17:03:43 +0900

    feat: Enable auto page tracking

--- _gtag.tsx
+++ _gtag.tsx
@@ -5,6 +5,16 @@ export interface GtagProps {
 }
 
 const Gtag = ({ id }: GtagProps) => {
+  // Pagic dispatches `rerender` event: https://github.com/xcatliu/pagic/blob/v1.4.0/src/plugins/script_index.js#L99
+  React.useEffect(() => {
+    // deno-lint-ignore no-explicit-any
+    const gtag = (window as any).gtag;
+    if (gtag) {
+      gtag("set", "page_path", window.location.pathname);
+      gtag("event", "page_view");
+    }
+  }, [id]);
+
   return (
     <>
       <script async src={`https://www.googletagmanager.com/gtag/js?id=${id}`} />
