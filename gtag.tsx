4a126b8da0d34acbf42dad1af648a94c22a2ff73
uki00a <uki00a@gmail.com>
2021-08-21 16:40:52 +0900

    fix: Fotgot to add gtag.tsx

---
+++ gtag.tsx
@@ -0,0 +1,21 @@
+export interface GtagProps {
+  id: string;
+}
+
+const Gtag = ({ id }: GtagProps) => {
+  return (
+    <>
+      <script async src={`https://www.googletagmanager.com/gtag/js?id=${id}`} />
+      <script
+        dangerouslySetInnerHTML={{
+          __html: `window.dataLayer = window.dataLayer || [];
+            function gtag(){dataLayer.push(arguments);}
+            gtag('js', new Date());
+            gtag('config', '${id}');`,
+        }}
+      />
+    </>
+  );
+};
+
+export default Gtag;
