76b82bb24ba90569f045b39a5778fe1f0d94d969
uki00a <uki00a@users.noreply.github.com>
2021-09-20 08:28:48 +0000

    deploy: f80e566dad973f50cc577e35c11fc84050111cce

--- _layout.js
+++
@@ -1,5 +0,0 @@
-import LayoutBase from './_layout_base.js';
-import Main from './_main.js';
-import Posts from './posts/_posts.js';
-const Layout = (props) => (React.createElement(LayoutBase, Object.assign({}, props, { Main: props.outputPath === 'index.html' ? Posts : Main })));
-export default Layout;
--- dev.ts
+++
@@ -1,8 +0,0 @@
-import Pagic from "pagic";
-
-if (import.meta.main) {
-  await new Pagic({
-    watch: true,
-    serve: true,
-  }).build();
-}
